3f1a43c33e9e0723241353cc22a9fa41be703c62
rafasc <m8821@alunos.uevora.pt>
2017-09-16 03:09:00 +0100

    Update websocket tests
    Previous test crashed when trying to connect to the websocket.
    test was rewritten using `faye/websocket`.
    
    Fixes ianks/octodown#92

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,50 +1,44 @@
+require 'net/http'
+require 'faye/websocket'
+require 'eventmachine'
+
 describe 'Integration' do
-  it 'actually runs...' do
-    begin
-      pid = fork do
-        octodown = File.join(__dir__, '..', 'bin', 'octodown')
-        exec "TEST=1 bundle exec #{octodown} --live-reload #{dummy_path}"
-      end
-
-      req = Net::HTTP::Get.new('/')
-
-      sleep 2
-      res = Net::HTTP.start 'localhost', 8080 do |http|
-        http.request(req)
-      end
-
-      body = res.body
-
-      expect(body).to include 'You are now reading markdown. How lucky you are!'
-
-    ensure
-      Process.kill('TERM', pid)
-      Process.wait pid
+  before :all do
+    @pid = fork do
+      octodown = File.join(__dir__, '..', 'bin', 'octodown')
+      exec "TEST=1 bundle exec #{octodown} --live-reload #{dummy_path}"
     end
+    sleep 2
   end
 
-  it 'fires up a websocket' do
-    begin
-      pid = fork do
-        octodown = File.join(__dir__, '..', 'bin', 'octodown')
-        exec "TEST=1 bundle exec #{octodown} --live-reload #{dummy_path}"
-      end
+  after :all do
+    Process.kill('TERM', @pid)
+    Process.wait @pid
+  end
 
-      req = Net::HTTP::Get.new('/')
-      req['Upgrade'] = 'websocket'
-      req['Connection'] = 'Upgrade'
-      req['Origin'] = 'http://localhost'
-
-      sleep 2
-
-      res = Net::HTTP.start 'localhost', 8080 do |http|
-        http.request(req)
-      end
-
-      expect(res).to be_a Net::HTTPSwitchProtocol
-    ensure
-      Process.kill('TERM', pid)
-      Process.wait pid
+  it 'runs and serves the files over http' do
+    res = Net::HTTP.start 'localhost', 8080 do |http|
+      http.request(Net::HTTP::Get.new('/'))
     end
+    expect(res.body).to include 'You are now reading markdown.'\
+                                ' How lucky you are!'
+  end
+
+  it 'runs and receives data from the websocket' do
+    message = nil
+    EM.run do
+      ws = Faye::WebSocket::Client.new('ws://localhost:8080/')
+      ws.add_event_listener('message', lambda { |e| message = e.data })
+
+      start = Time.now
+      timer = EM.add_periodic_timer 0.1 do
+        if message || Time.now.to_i - start.to_i > 5
+          timer.cancel
+          EM.stop_event_loop
+        end
+      end
+    end
+    expect(message).to include('You are now reading markdown.'\
+                               ' How lucky you are!')
   end
 end
