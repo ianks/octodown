95494e9eea4b9479143f485f9b1be3748c7c5673
rafasc <m8821@alunos.uevora.pt>
2017-09-22 04:06:35 +0100

    Do not callback for removed events

--- lib/octodown/support/services/riposter.rb
+++ lib/octodown/support/services/riposter.rb
@@ -8,8 +8,8 @@ module Octodown
           path = File.dirname(File.expand_path(file.path))
           regex = Regexp.new("^#{File.basename(file.path)}$")
 
-          @listener ||= Listen.to(path, only: regex) do
-            listener_callback.call
+          @listener ||= Listen.to(path, only: regex) do |modified, added, _rm|
+            listener_callback.call if modified.any? || added.any?
           end
 
           @listener.start
