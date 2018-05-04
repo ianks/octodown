f58adb536ccb920ff383120dc1b03ee4158d9928
Ian Ker-Seymer <i.kerseymer@gmail.com>
2018-05-04 15:23:41 -0400

    Use git to choose files when available

--- lib/octodown/support/file_chooser.rb
+++ lib/octodown/support/file_chooser.rb
@@ -1,26 +1,11 @@
-class FileChooser
-  attr_reader :prompt
+require 'English'
+require 'tty-prompt'
 
-  def initialize(prompt:)
-    @prompt = prompt
-  end
+module Octodown
+  class FileChooser
+    TUI = ::TTY::Prompt.new(enable_color: true)
 
-  def choose_file
-    choices = all_markdown_files
-    choice = prompt.select('Which markdown file would you like to edit?', choices)
-    File.open(choice, 'r')
-  end
-
-  def abort_no_files_found!
-    prompt.error 'We could not find any markdown files in this folder.'
-    puts
-    prompt.error 'Try passing the file explicitly such as, i.e:'
-    prompt.error '    $ octodown README.md'
-    exit 1
-  end
-
-  def all_markdown_files
-    extensions = %w[markdown
+    EXTENSIONS = %w[markdown
                     mdown
                     mkdn
                     md
@@ -29,12 +14,77 @@ class FileChooser
                     mdtxt
                     mdtext
                     text
-                    Rmd]
+                    Rmd].freeze
 
-    choices = Dir.glob "**/*.{#{extensions.join(',')}}"
+    class MarkdownFileList
+      class Git
+        def call
+          ext_args = EXTENSIONS.map { |ext| "**/*.#{ext} *.#{ext}" }.join(' ')
+          `git ls-files --cached --others -z #{ext_args}`.split("\x0").uniq
+        end
 
-    abort_no_files_found! if choices.empty?
+        def runnable?
+          `git rev-parse --is-inside-work-tree`
+          $CHILD_STATUS.success?
+        rescue StandardError
+          false
+        end
+      end
 
-    choices.sort_by! { |c| c.split(File::SEPARATOR).length }
+      class Glob
+        def call
+          Dir.glob "*.{#{EXTENSIONS.join(',')}}"
+        end
+
+        def runnable?
+          true
+        end
+      end
+
+      attr_reader :logger
+
+      def initialize(logger)
+        @logger = logger
+      end
+
+      def call
+        logger.debug("File choose strategy: #{winning_strategy.class.name}")
+        winning_strategy.call
+      end
+
+      def winning_strategy
+        strats = [Git.new, Glob.new]
+        @winning_strategy ||= strats.find(&:runnable?)
+      end
+    end
+
+    attr_reader :prompt, :logger
+
+    def initialize(logger:)
+      @logger = logger
+      @prompt = TUI
+    end
+
+    def call
+      choices = all_markdown_files
+      choice = prompt.select('Which file would you like to edit?', choices)
+      File.open(choice, 'r')
+    end
+
+    def abort_no_files_found!
+      prompt.error 'We could not find any markdown files in this folder.'
+      puts
+      prompt.error 'Try passing the file explicitly such as, i.e:'
+      prompt.error '    $ octodown README.md'
+      exit 1
+    end
+
+    def all_markdown_files
+      choices = MarkdownFileList.new(logger).call
+
+      abort_no_files_found! if choices.empty?
+
+      choices.sort_by! { |c| c.split(File::SEPARATOR).length }
+    end
   end
 end
